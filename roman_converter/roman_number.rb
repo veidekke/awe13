ac1091a482a1dbc57b0adc53d875b1b412970c09
sbrink <sbrink@tzi.de>
2013-02-12 18:32:17 +0100

    started implementation check_roman_syntax + check_latin_syntax

--- roman_converter/roman_number.rb
+++ roman_converter/roman_number.rb
@@ -1,10 +1,10 @@
-class Roman_Number
+﻿class Roman_Number
   MAP = {
-    1 => "I", 
-    4 => "IV", 
-    5 => "V", 
-    9 => "IX", 
-    10 => "X", 
+    1 => "I",
+    4 => "IV",
+    5 => "V",
+    9 => "IX",
+    10 => "X",
     40 => "XL",
     50 => "L",
     90 => "XC",
@@ -14,17 +14,36 @@ class Roman_Number
     900 => "CM",
     1000 => "M"
     }
-  
+
   def from_roman_number(roman)
-  
+
   end
-  
+
   def to_roman_number(latin)
-    
+
   end
-  
-  def check_syntax(roman)
-	
+
+  # Returns false if roman is no valid roman number.
+  # (Return to return immediately)
+  def check_roman_syntax(roman)
+    roman.upcase!
+    roman.each_char { |c|
+                        if !(MAP.has_value? c)
+                            puts "#{c} ist kein gueltiges roemisches Literal."
+                            return false
+                        end
+                    }
+
+    if ((roman.include? "IIII") || (roman.include? "XXXX") || (roman.include? "CCCC") || (roman.include? "MMMM") || (roman.include? "VV") || (roman.include? "LL") || (roman.include? "DD"))
+        puts "Ein Literal tritt zu oft hintereinander auf."
+        return false
+    end
+
+    true
   end
-  
+
+  def check_latin_syntax(latin)
+    !latin.match(/[^0-9]/) && latin.to_i >= 0
+  end
+
 end
