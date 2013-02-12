bd66f777dfcbf47386adbc8babe322fa3c2b1e18
Eike Behrends <eikebehrends@Eike-Behrendss-MacBook-Pro.local>
2013-02-12 15:35:03 +0100

    Grundgeruest

---
+++ roman_converter/roman_number.rb
@@ -0,0 +1,25 @@
+class Roman_Number
+  MAP = {
+    1 => "I", 
+    4 => "IV", 
+    5 => "V", 
+    9 => "IX", 
+    10 => "X", 
+    40 => "XL",
+    50 => "X",
+    90 => "XC",
+    100 => "C",
+    400 => "CD",
+    500 => "D",
+    900 => "CM",
+    1000 => "M"
+    }
+  
+  def from_roman_number(char)
+  
+  end
+  
+  def to_roman_number(int)
+    
+  end
+end
---
+++ roman_converter/test_roman_number.rb
@@ -0,0 +1,6 @@
+require "./roman_number"
+require "test/unit"
+
+class Roman_number_test < Test::Unit::TestCase
+
+end
