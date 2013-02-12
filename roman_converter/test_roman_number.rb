43946a9efd33cd2c436ab488febbf6b2aaaa9fd1
Eike Behrends <eikebehrends@Eike-Behrendss-MacBook-Pro.local>
2013-02-12 15:46:39 +0100

    Gerueste der Testmethoden

--- roman_converter/test_roman_number.rb
+++ roman_converter/test_roman_number.rb
@@ -2,5 +2,39 @@ require "./roman_number"
 require "test/unit"
 
 class Roman_number_test < Test::Unit::TestCase
-
+  #Testen von to_roman_number
+  def test_simple_latin_numbers
+  
+  end
+  
+  def test_complecate_latin_numbers_without_subtraction
+    
+  end
+  
+  
+  def test_complecate_latin_numbers_with_subtraction
+    
+  end
+  
+  def test_special_cases_latin_numbers
+    
+  end
+  
+  #Testen von from_roman_number
+  def test_simple_roman_numbers
+  
+  end
+  
+  def test_complecate_roman_numbers_without_subtraction
+  
+  end
+  
+  def test_complecate_roman_numbers_with_subtraction
+    
+  end
+  
+  def test_special_cases_roman_numbers
+    
+  end
+  
 end
