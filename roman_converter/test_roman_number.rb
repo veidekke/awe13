1f0deb293151b757279039358dfac9e78e42b387
Eike Behrends <eikebehrends@Eike-Behrendss-MacBook-Pro.local>
2013-02-13 14:48:57 +0100

    weitere Testmethoden

--- roman_converter/test_roman_number.rb
+++ roman_converter/test_roman_number.rb
@@ -37,19 +37,33 @@ class Roman_number_test < Test::Unit::TestCase
   
   #Testen von from_roman_number
   def test_simple_roman_numbers
-  
+    assert_equal(1, @roman_number.from_roman_number("I"))
+    assert_equal(5, @roman_number.from_roman_number("V"))
+    assert_equal(10, @roman_number.from_roman_number("X"))
+    assert_equal(50, @roman_number.from_roman_number("L"))
+    assert_equal(100, @roman_number.from_roman_number("C"))
+    assert_equal(500, @roman_number.from_roman_number("D"))
+    assert_equal(1000, @roman_number.from_roman_number("M"))
   end
   
   def test_complecate_roman_numbers_without_subtraction
-  
+    assert_equal("II", @roman_number.from_roman_number(2))
+    assert_equal("VI", @roman_number.from_roman_number(6))
+    assert_equal("XVI", @roman_number.from_roman_number(16))
+    assert_equal("DCL", @roman_number.from_roman_number(650))
+    assert_equal("MMDCLXI", @roman_number.from_roman_number(2661))
   end
   
   def test_complecate_roman_numbers_with_subtraction
+    assert_equal("IV", @roman_number.from_roman_number(4))
+    assert_equal("MCMLI", @roman_number.from_roman_number(1951))
+    assert_equal("CDXXXIV", @roman_number.from_roman_number(434))
     
   end
   
   def test_special_cases_roman_numbers
-    
+    assert_equal(nil, @roman_number.to_roman_number("IIII"))
+    assert_equal(nil, @roman_number.to_roman_number("IIV"))
   end
   
 end
